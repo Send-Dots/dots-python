21a76b1b32a155a2645eea54f76415ff5a150278
Kartikye Mittal <kartikye@getathelas.com>
2021-06-15 14:28:25 -0700

    1.0.0

---
+++ build/lib/dots/invoice.py
@@ -0,0 +1,45 @@
+import base64
+import requests
+
+import dots
+from dots import token
+
+class Invoice():
+
+    @classmethod
+    def create(cls, amount, expires_in, items, breakdown, requested_information):
+        
+        json = {
+            'amount': amount,
+            'expires_in': expires_in,
+            'items': items,
+            'breakdown': breakdown,
+            'requested_information': requested_information,
+        }
+
+        headers = {
+            'Authorization': 'Basic ' + token.get_auth_token()
+        }
+
+        response = requests.post(dots.api_base + '/invoice/create', json=json, headers=headers)
+        data = response.json()
+
+        if data['success']:
+            return data['invoice']
+        else:
+            response.raise_for_status()
+
+    @classmethod
+    def get(cls, invoice_id):
+        
+        headers = {
+            'Authorization': 'Basic ' + token.get_auth_token()
+        }
+
+        response = requests.get(dots.api_base + '/invoice/get/' + invoice_id, headers=headers)
+        data = response.json()
+
+        if data['success']:
+            return data['invoice']
+        else:
+            response.raise_for_status()
