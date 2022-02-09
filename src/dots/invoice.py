caa433a486547fb1003ba17ae0223f31d8f5022e
Jack <jxl7@cornell.edu>
2022-02-09 11:27:16 -0800

    modified invoice and added payout link

--- src/dots/invoice.py
+++ src/dots/invoice.py
@@ -4,32 +4,37 @@ import requests
 import dots
 from dots import token
 
+
 class Invoice():
 
     @classmethod
-    def create(cls, amount, expires_in=None, items=None, breakdown=None, requested_information=None):
-        
+    def create(cls, amount, expires_in=None, items=None, breakdown=None, requested_information=None, metadata=None):
+
         json = {
             'amount': amount,
         }
 
         if expires_in is not None:
             json['expires_in'] = expires_in
-        
+
         if items is not None:
             json['items'] = items
-        
+
         if breakdown is not None:
             json['breakdown'] = breakdown
-        
+
         if requested_information is not None:
             json['requested_information'] = requested_information
+
+        if metadata is not None:
+            json['metadata'] = metadata
 
         headers = {
             'Authorization': 'Basic ' + token.get_auth_token()
         }
 
-        response = requests.post(dots.api_base + '/invoice/create', json=json, headers=headers)
+        response = requests.post(
+            dots.api_base + '/invoice/create', json=json, headers=headers)
 
         data = response.json()
 
@@ -40,12 +45,13 @@ class Invoice():
 
     @classmethod
     def get(cls, invoice_id):
-        
+
         headers = {
             'Authorization': 'Basic ' + token.get_auth_token()
         }
 
-        response = requests.get(dots.api_base + '/invoice/get/' + invoice_id, headers=headers)
+        response = requests.get(
+            dots.api_base + '/invoice/get/' + invoice_id, headers=headers)
         data = response.json()
 
         if data['success']:
